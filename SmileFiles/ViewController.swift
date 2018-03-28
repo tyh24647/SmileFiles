f65268acbde0966bb38aba38fee2e94f1b608b0b
Tyler Hostager <tyhostager@gmail.com>
2018-03-28 15:57:27 -0500

    Initial Commit

---
+++ SmileFiles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmileFiles
+//
+//  Created by Tyler hostager on 3/28/18.
+//  Copyright © 2018 Tyler hostager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SmileFilesTests/SmileFilesTests.swift
@@ -0,0 +1,36 @@
+//
+//  SmileFilesTests.swift
+//  SmileFilesTests
+//
+//  Created by Tyler hostager on 3/28/18.
+//  Copyright © 2018 Tyler hostager. All rights reserved.
+//
+
+import XCTest
+@testable import SmileFiles
+
+class SmileFilesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
